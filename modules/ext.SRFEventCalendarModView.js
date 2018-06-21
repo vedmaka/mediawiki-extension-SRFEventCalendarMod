c0203049355220619559ddef805873a4858b5e33
Vedmaka <god.vedmaka@gmail.com>
2018-06-21 23:24:47 +0300

    Hide center and right toolbars on view activation

--- modules/ext.SRFEventCalendarModView.js
+++ modules/ext.SRFEventCalendarModView.js
@@ -4,14 +4,26 @@
 
 	SRFEventCalendarView = View.extend( {
 
+		initialize: function () {
+			// called once when the view is instantiated, when the user switches to the view.
+			// initialize member variables or do other setup tasks.
+		},
+
 		render: function () {
 			this.el.html( '<div class="srf-loading-dots"></div>' ).load( this.options.views.srfeventcalendarview.buttonLink );
+			this.calendar.el.find( '.fc-center' ).hide();
+			this.calendar.el.find( '.fc-right' ).hide();
 		},
 
 		setHeight: function ( height, isAuto ) {
 			// responsible for adjusting the pixel-height of the view. if isAuto is true, the
 			// view may be its natural height, and `height` becomes merely a suggestion.
 			this.el.css( 'height', height + 'px' );
+		},
+
+		destroy: function () {
+			this.calendar.el.find( '.fc-center' ).show();
+			this.calendar.el.find( '.fc-right' ).show();
 		}
 
 	} );
